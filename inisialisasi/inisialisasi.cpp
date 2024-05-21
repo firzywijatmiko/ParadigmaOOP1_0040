9b65191756fef1451fde6a58ddd917130444fb93
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-21 17:52:05 +0700

    Add project files.

---
+++ inisialisasi/inisialisasi.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//ripositori : pradigmaoop1-
+// minimal comit 19
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pid, string pNama, float pNilai) :id(pid), nama(pNama), nilai(pNilai){
+		//definisi
+
+	}
+
+	~mahasiswa() {
+		cout << "id  = " << id << endl;
+		cout << "nama = " << nama << endl;
+		cout << "nilai = " << nilai << endl;
+	}
+
+
+};
+
+int main() {
+	mahasiswa mhs(12, "Faris", 90.5);
+
+	return 0;
+}
+
