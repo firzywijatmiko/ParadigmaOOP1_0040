b0a7f21510c7009bb88f80e934273bbad389bbc9
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-21 19:39:17 +0700

    nama anak

--- agregasi/anak-h.h
+++ agregasi/anak-h.h
@@ -1,8 +1,14 @@
 #ifndef ANAK_H
 #ifndef ANAK_H
 
-class anak{
+class anak {
 public:
 	string nama;
 	anak(string pNama) :nama(pNnama) {
-
+		cout << "anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
