5aa0df0acf25b061322ec3e9b270eea6496a1238
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-21 21:24:18 +0700

    perkenalan

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -6,4 +6,30 @@ class orang {
 public:
 	string nama;
 
-	orang
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n
+			\n"}
+	};
